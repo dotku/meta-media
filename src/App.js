4557458bd889b245d6bbe7279d7b37057ff3ec76
Weijing Jay Lin <weijingjaylin@gmail.com>
2022-10-11 09:38:36 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <center>
+        <a href="https://metamedia.us.com">
+          <div class="main meta-font">
+            <div class="logo meta-font">=OO=</div>
+            <div class="title">MetaMedia</div>
+            <div class="">metamedia.us.com</div>
+          </div>
+        </a>
+      </center>
+    </div>
+  );
+}
